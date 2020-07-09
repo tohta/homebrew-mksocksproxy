28cd24566c2e4aa2355eddde783c7b56df3efbb5
tohta <ohtatakeshi@gmail.com>
2020-07-10 00:24:20 +0900

    bump up version

--- mksocksproxy.rb
+++ mksocksproxy.rb
@@ -1,11 +1,11 @@
 REPOSITORY_URL="https://github.com/tohta/mksocksproxy".freeze
-HOMEBREW_GITPULLS_VERSION="0.0.3".freeze
+HOMEBREW_GITPULLS_VERSION="0.0.4".freeze
 
 class Mksocksproxy < Formula
   desc "mksocksproxy"
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
-  sha256 "e6c31eee2c426756ea28ed2f29ecbf5de60ac0b069afebae47e977fb2b6a5c0f"
+  sha256 "2918b81b8aaea52941c2d181ec22557b44f12e79ee83153dbec64b44ab5e9f3c"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
   def install
